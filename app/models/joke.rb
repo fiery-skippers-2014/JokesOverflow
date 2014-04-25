7e65ac278bfd4f36224e46fc5116c32608d81d26
Nathan Sass <nsass711@gmail.com>
2014-04-25 12:14:18 -0700

    joke model & feature tests

--- app/models/joke.rb
+++ app/models/joke.rb
@@ -1,4 +1,5 @@
 class Joke < ActiveRecord::Base
   attr_accessible :title
   has_many :replies
+  validates :title, presence: true, uniqueness: true
 end
---
+++ spec/features/can_create_new_joke.rb
@@ -0,0 +1,49 @@
+require 'spec_helper'
+
+feature 'can create new joke'  do
+  context 'from the home page' do
+    let(:joke) {FactoryGirl.attributes_for (:joke)}
+    it 'create a new joke' do
+      visit root_path
+      click_on 'New Joke'
+      fill_in 'joke[title]', :with => joke[:title]
+      click_on 'Create Joke'
+      page.should have_content joke[:title]
+    end
+
+    it 'doesn\'t create two of the same jokes' do
+      visit root_path
+      click_on 'New Joke'
+      fill_in 'joke[title]', :with => "bob"
+      click_on 'Create Joke'
+      click_on 'New Joke'
+      fill_in 'joke[title]', :with => "bob"
+      click_on 'Create Joke'
+      page.should have_content "Create New Joke"
+    end
+
+
+    it 'change joke count by one' do
+      expect {
+      visit root_path
+      click_on 'New Joke'
+      fill_in 'joke[title]', :with => joke[:title]
+      click_on 'Create Joke'
+       }.to change(Joke, :count).by(1)
+    end
+
+    it 'you can click on each joke' do
+      visit root_path
+      click_on 'New Joke'
+      fill_in 'joke[title]', :with => "Fake Joke"
+      click_on 'Create Joke'
+      click_on 'Fake Joke'
+      page.should have_content 'Fake Joke'
+    end
+
+  end
+
+
+
+end
+
