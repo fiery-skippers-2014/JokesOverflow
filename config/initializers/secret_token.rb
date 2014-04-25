3367e4a6a43870a5aeab8e8edcc54dc5ed51a811
Erin Joan Snyder & Lyndsey Williams <pair+ErinJoan.LyndseyWilliams@devbootcamp.com>
2014-04-24 17:08:41 -0700

    added skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JokesOverflow::Application.config.secret_token = '18a726ec0196f98c4d4cfabd32cdf7e56448b8a367b54a27e613c70abc5ca225ca15765e2a96d060c85e75c2323ca2c107e8ff0404e8c8fc4715700f4047a318'
