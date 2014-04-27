bd2a671b2782e170434e9e0bdc2549544e5cb16e
Nathan Sass <nsass711@gmail.com>
2014-04-26 21:14:23 -0700

    cookies works and flash message dissapears

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,11 +1,11 @@
 
-
 class VotesController < ApplicationController
   def create
-    cookies[params[:id]] ||= false
+    flash.clear
+    cookies[params[:id]] ||= { value: false, expires: 10.minutes.from_now }
     unless cookies[params[:id]]
       up_vote = params[:up]
-      cookies[params[:id]] = true
+      cookies[params[:id]] = { value: true, expires: 10.minutes.from_now }
       @votes =  Vote.find_vote_count(params[:id], up_vote)
     else
       flash[:notice] = "You cannot Vote Again"
@@ -14,3 +14,18 @@ class VotesController < ApplicationController
     render :partial => "shared/votescount", local: { votes: @votes }
   end
 end
+
+# class VotesController < ApplicationController
+#   def create
+#     cookies[params[:id]] ||= false
+#     unless cookies[params[:id]]
+#       up_vote = params[:up]
+#       cookies[params[:id]] = true
+#       @votes =  Vote.find_vote_count(params[:id], up_vote)
+#     else
+#       flash[:notice] = "You cannot Vote Again"
+#       @votes =  Joke.find(params[:id]).votes.length
+#     end
+#     render :partial => "shared/votescount", local: { votes: @votes }
+#   end
+# end
