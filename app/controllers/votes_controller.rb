d39a83011fa215f7f1b3c2f156804730b223bc64
Nathan Sass <nsass711@gmail.com>
2014-04-26 17:31:23 -0700

    refactoring votes controller

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,44 +1,15 @@
-# class VotesController < ActionController::Base
 
-#   def new
-#     @vote = Vote.new
-#   end
-
-#   def create
-#     if params[:up] == "true"
-#       if params[:joke_id] != nil
-#         @reply = Reply.find(params[:id])
-#         @reply.votes << Vote.create
-#       else
-#         @joke = Joke.find(params[:id])
-#         @joke.votes << Vote.create
-#       end
-#     else
-#       if params[:joke_id] != nil
-#         @reply = Reply.find(params[:id])
-#         @reply.votes.last.destroy
-#       else
-#         @joke = Joke.find(params[:id])
-#         @joke.votes.last.destroy
-#         p @joke.votes
-#       end
-#     end
-#     # render :partial => "shared/votescount", local: {votes: @votes}
-#   end
-
-# end
-
-# require 'debugger'
 
 class VotesController < ApplicationController
   def create
-    @votes = 0
+    # @votes = 0
     p "$ "*30
-    p params
+    p poo
     if params[:up] == "true"
       if params[:joke_id] != nil
         @reply = Reply.find(params[:id])
         @reply.votes << Vote.create
+        @votes = @reply.votes.length
       else
         @joke = Joke.find(params[:id])
         @joke.votes << Vote.create
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,3 +1,8 @@
 class Vote < ActiveRecord::Base
   belongs_to :votable, :polymorphic => true
+
+  def self.poo
+    puts " $ # @" * 30
+    puts "in a pooey mess"
+  end
 end
