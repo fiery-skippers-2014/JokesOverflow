23f88d54f44b6aa7379d8678653fcef83618d00f
Nathan Sass <nsass711@gmail.com>
2014-04-26 22:04:28 -0700

    cookies

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -2,10 +2,10 @@
 class VotesController < ApplicationController
   def create
     flash.clear
-    cookies[params[:id]] ||= { value: false, expires: 10.minutes.from_now }
+    cookies[params[:id]] ||= { value: false, expires: 10.seconds.from_now }
     unless cookies[params[:id]]
       up_vote = params[:up]
-      cookies[params[:id]] = { value: true, expires: 10.minutes.from_now }
+      cookies[params[:id]] = { value: true, expires: 10.seconds.from_now }
       @votes =  Vote.find_vote_count(params[:id], up_vote)
     else
       flash[:notice] = "You cannot Vote Again"
@@ -15,17 +15,3 @@ class VotesController < ApplicationController
   end
 end
 
-# class VotesController < ApplicationController
-#   def create
-#     cookies[params[:id]] ||= false
-#     unless cookies[params[:id]]
-#       up_vote = params[:up]
-#       cookies[params[:id]] = true
-#       @votes =  Vote.find_vote_count(params[:id], up_vote)
-#     else
-#       flash[:notice] = "You cannot Vote Again"
-#       @votes =  Joke.find(params[:id]).votes.length
-#     end
-#     render :partial => "shared/votescount", local: { votes: @votes }
-#   end
-# end
